35874d91fc5a11f0034fad42c95f9b29b9908905
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-25 15:18:07 +0700

    add counter setup

---
+++ src/store/actions/counter.js
@@ -0,0 +1,12 @@
+import { INCREMENT, DECREMENT } from '../types'
+
+
+export const increaseNumber = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+export const decreaseNumber = () => ({
+    type: DECREMENT
+})
