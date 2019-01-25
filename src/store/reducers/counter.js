1819d7832c245e425be1477a3b8bb9d62b28203a
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-25 15:41:12 +0700

    best practices default state

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,11 +1,13 @@
 import { INCREMENT, DECREMENT } from "../types";
 
-export default (state = { number: 0, dataType: 'integer'}, action) => {
+const initialState = { number: 0, dataType: "integer" };
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { ...state, number: state.number + 1}
+      return { ...state, number: state.number + 1 };
     case DECREMENT:
-      return { ...state, number: state.number - 1}
+      return { ...state, number: state.number - 1 };
     default:
       return state;
   }
